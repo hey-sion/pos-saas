7b25c6141423f5b9cb2e17481c0d274433db4b23
hey-sion <heysion007005@gmail.com>
2026-05-09 15:33:07 +0900

    refactor: api 엔드포인트에서 pos 네이밍 제거

--- src/main/java/com/sion/pos/interfaces/web/PosPageController.java
+++ src/main/java/com/sion/pos/interfaces/web/PosPageController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PosPageController {
 
-    @GetMapping({"/", "/pos"})
+    @GetMapping("/")
     public String pos() {
         return "pos/index";
     }
 
-    @GetMapping("/pos/manage")
+    @GetMapping("/manage")
     public String manage() {
         return "pos/manage";
     }
